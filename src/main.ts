68cf9c38bbba21582699220d6a4b829864063856
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2023-07-18 14:12:39 +0300

    docs: add OpenAPI-spec-compliant docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Author: Authentication, Authorization & User Management.')
+    .setDescription('API documentation for Author')
+    .setVersion('1.0')
+    .addTag('Author')
+    .build();
+
+  const docsOptions: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, docsOptions);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
