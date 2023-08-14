3b10c8ae3fd9bb57440b050dec42aea7bc764701
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2023-08-14 08:49:56 +0300

    db(schema | migrations): create Providers & UserProviderRealations models

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,5 +1,9 @@
 import users from './users';
+import providers from './providers';
+import userProviderRelations from './userProviderRelations';
 
 export default {
   users,
+  providers,
+  userProviderRelations,
 };
